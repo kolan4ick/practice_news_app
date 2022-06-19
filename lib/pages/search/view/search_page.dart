bfe487d98513492cf1982be2466ec28db69b5ea2
Kolan <kolan4ick@gmail.com>
2022-06-20 00:02:09 +0300

    Base search implementation

--- lib/pages/search/view/search_page.dart
+++ lib/pages/search/view/search_page.dart
@@ -15,7 +15,57 @@ class _SearchPageState extends State<SearchPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Пошук'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              showSearch(context: context, delegate: MySearchDelegate());
+            },
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class MySearchDelegate extends SearchDelegate<String> {
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          if (query.isNotEmpty) {
+            query = '';
+          } else {
+            close(context, "null");
+          }
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, "null");
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return const Center(
+      child: Text('Результати пошуку'),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return const Center(
+      child: Text('Пропозиції пошуку'),
     );
   }
 }
